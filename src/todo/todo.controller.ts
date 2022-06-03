3d870813b730924a691ea8295ab257bc35ddc0e5
hao0731 <ss501801@gmail.com>
2022-06-03 20:03:49 +0800

    feat(todo): 新增查詢參數範例

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,23 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('todos')
 export class TodoController {
-  @Get(':id')
-  getTodo(@Param() params: { id: string }) {
-    const { id } = params;
-    return {
-      id,
-      title: `Title ${id}`,
-      description: '',
-    };
+  @Get()
+  getQuery(@Query() query: { limit: string; skip: string }) {
+    return query;
   }
 
-  // @Get(':id')
-  // getTodo(@Param('id') id: string) {
-  //   return {
-  //     id,
-  //     title: `Title ${id}`,
-  //     description: '',
-  //   };
+  // @Get()
+  // getQuery(@Query('limit') limit: string, @Query('skip') skip: string) {
+  //   return { limit, skip };
   // }
 }
