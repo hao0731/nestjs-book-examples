95a140f04aaf64e788d9e04cd3f6969d216b717e
hao0731 <ss501801@gmail.com>
2022-06-03 20:47:17 +0800

    feat(ttodo): 新增回應處理範例

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,10 +1,47 @@
-import { Controller, Header, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { of } from 'rxjs';
 
 @Controller('todos')
 export class TodoController {
+  /** Promise */
   @Get()
-  @Header('X-Hao-headers', '1')
   getTodos() {
-    return [];
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve([]), 1000);
+    });
   }
+
+  /** async/await */
+  // @Get()
+  // async getTodos() {
+  //   const todos = await new Promise((resolve, reject) => {
+  //     setTimeout(() => resolve([]), 1000);
+  //   });
+  //   return todos;
+  // }
+
+  /** RxJS */
+  // @Get()
+  // getTodos() {
+  //   return of([]);
+  // }
+
+  /** Library response */
+  // @Get()
+  // getTodos(@Res() res: Response) {
+  //   res.send([]);
+  // }
+
+  /** Invalid response */
+  // @Get()
+  // getTodos(@Res() res: Response) {
+  //   return [];
+  // }
+
+  /** Hybrid response */
+  // @Get()
+  // getTodos(@Res({ passthrough: true }) res: Response) {
+  //   return [];
+  // }
 }
