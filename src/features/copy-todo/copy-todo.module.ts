081eef00d33a4175870d68c6729e99ef5b727727
hao0731 <ss501801@gmail.com>
2022-06-05 16:19:30 +0800

    feat: 新增全域模組範例

--- src/features/copy-todo/copy-todo.module.ts
+++ src/features/copy-todo/copy-todo.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TodoModule } from '../todo/todo.module';
 import { CopyTodoController } from './copy-todo.controller';
 
 @Module({
-  imports: [TodoModule],
   controllers: [CopyTodoController],
 })
 export class CopyTodoModule {}
--- src/features/todo/todo.module.ts
+++ src/features/todo/todo.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
+@Global()
 @Module({
   controllers: [TodoController],
   providers: [TodoService],
