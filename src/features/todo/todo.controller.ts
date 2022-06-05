00815b8b5403b0e2fc5dfba25b7b06109f95b961
hao0731 <ss501801@gmail.com>
2022-06-05 15:52:52 +0800

    新增功能模組範例

--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('todos')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  getTodos() {
+    return this.todoService.getTodos();
+  }
+}
--- src/features/todo/todo.service.ts
+++ src/features/todo/todo.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+export interface ITodo {
+  id: number;
+  title: string;
+  description: string;
+}
+
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  private todos: ITodo[] = [
+    {
+      id: 1,
+      title: 'Title 1',
+      description: '',
+    },
+  ];
+
+  getTodos() {
+    return this.todos;
+  }
+}
