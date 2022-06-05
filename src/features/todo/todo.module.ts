af07caaf8b408b0710d3b99384be47c03f4216e6
hao0731 <ss501801@gmail.com>
2022-06-05 15:33:04 +0800

    feat(todo): 新增 TodoController

--- src/features/todo/todo.module.ts
+++ src/features/todo/todo.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TodoController } from './todo.controller';
 
-@Module({})
+@Module({
+  controllers: [TodoController]
+})
 export class TodoModule {}
