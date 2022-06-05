07b6498bd1e3f1a4532bfc30419a2f42148e752d
hao0731 <ss501801@gmail.com>
2022-06-05 16:03:36 +0800

    feat(copy-todo): 新增CopyTodoModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './features/todo/todo.module';
+import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, CopyTodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
