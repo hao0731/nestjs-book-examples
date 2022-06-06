a493fd51fabdfb26e1f2ada65538f1bca1c3017d
hao0731 <ss501801@gmail.com>
2022-06-06 22:01:24 +0800

    feat: 新增標準Exception範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const status = HttpStatus.BAD_REQUEST;
+    const message = '出錯囉！';
+    // 將 HttpException 第一個參數換成 override 即可覆蓋預設格式
+    const override = { code: status, msg: message };
+    throw new HttpException(message, status);
     return this.appService.getHello();
   }
 }
