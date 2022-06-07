82fc320fa78da466814e214e7b8e001f685bdbe8
hao0731 <ss501801@gmail.com>
2022-06-07 20:11:34 +0800

    feat: 新增全域Exception filter範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  UseFilters,
-} from '@nestjs/common';
+import { BadRequestException, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseFilters(HttpExceptionFilter)
   @Get()
   getHello(): string {
-    // throw new HttpException('出錯囉！', HttpStatus.BAD_REQUEST);
     throw new BadRequestException('出錯囉！');
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
