35c9acbb41e6870742b31f1ea5e51ae7b542a3f2
hao0731 <ss501801@gmail.com>
2022-06-07 20:51:43 +0800

    feat: 新增內建Pipe範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.appService.getUser(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUser(id: number) {
+    const users = [
+      {
+        id: 1,
+        name: 'HAO',
+      },
+    ];
+    const user = users.find((x) => x.id === id);
+    return user || {};
   }
 }
