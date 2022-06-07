ed8c18af7d8338bc3eed9ae31dbe19387e0a1bcd
hao0731 <ss501801@gmail.com>
2022-06-07 21:34:45 +0800

    feat: 新增ValidationPipe自訂Exception範例

--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -1,6 +1,8 @@
 import {
   Body,
   Controller,
+  HttpStatus,
+  NotAcceptableException,
   Post,
   UsePipes,
   ValidationPipe,
@@ -10,7 +12,16 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 @Controller('todos')
 export class TodoController {
   @Post()
-  @UsePipes(new ValidationPipe({ disableErrorMessages: true }))
+  @UsePipes(
+    new ValidationPipe({
+      exceptionFactory: () => {
+        return new NotAcceptableException({
+          code: HttpStatus.NOT_ACCEPTABLE,
+          message: '格式錯誤',
+        });
+      },
+    }),
+  )
   createTodo(@Body() dto: CreateTodoDto) {
     return { id: 1, ...dto };
   }
