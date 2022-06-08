73b2a34f5e601dbf5be4c00e8387c70c5cf12900
hao0731 <ss501801@gmail.com>
2022-06-08 20:01:56 +0800

    feat: 新增OmitType範例

--- src/features/todo/dto/update-todo.dto.ts
+++ src/features/todo/dto/update-todo.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types';
+import { OmitType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PickType(CreateTodoDto, ['title']) {}
+export class UpdateTodoDto extends OmitType(CreateTodoDto, ['title']) {}
--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -11,7 +11,7 @@ import { UpdateTodoDto } from './dto/update-todo.dto';
 @Controller('todos')
 export class TodoController {
   @Patch(':id')
-  @UsePipes(ValidationPipe)
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   updateTodo(@Param('id') id: number, @Body() dto: UpdateTodoDto) {
     return { id, ...dto };
   }
