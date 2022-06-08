b036fafd0708bf51ca0c0c1dbc2897f0c4a16d45
hao0731 <ss501801@gmail.com>
2022-06-08 19:40:22 +0800

    feat: 新增ParseArrayPipe範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ParseIntPipe } from './pipes/parse-int.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get(':id')
-  getUser(
-    @Param('id', ParseIntPipe)
-    id: number,
-  ) {
-    return this.appService.getUser(id);
-  }
 }
--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -2,26 +2,27 @@ import {
   Body,
   Controller,
   Get,
-  Param,
+  ParseArrayPipe,
   Post,
-  UsePipes,
-  ValidationPipe,
+  Query,
 } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodoController {
   @Post()
-  @UsePipes(new ValidationPipe({ transform: true }))
-  createTodo(@Body() dto: CreateTodoDto) {
-    console.log(dto);
-    return dto;
+  createTodo(
+    @Body(new ParseArrayPipe({ items: CreateTodoDto }))
+    dtos: CreateTodoDto[],
+  ) {
+    return dtos;
   }
 
-  @Get(':id')
-  @UsePipes(new ValidationPipe({ transform: true }))
-  getTodo(@Param('id') id: number) {
-    console.log(typeof id);
-    return '';
+  @Get()
+  getTodos(
+    @Query('ids', new ParseArrayPipe({ items: Number, separator: ',' }))
+    ids: number[],
+  ) {
+    return ids;
   }
 }
