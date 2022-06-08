8a366ad765bb19094256e23de221f2298e83241c
hao0731 <ss501801@gmail.com>
2022-06-08 19:52:48 +0800

    feat: 新增PickType範例

--- src/features/todo/dto/update-todo.dto.ts
+++ src/features/todo/dto/update-todo.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PickType(CreateTodoDto, ['title']) {}
