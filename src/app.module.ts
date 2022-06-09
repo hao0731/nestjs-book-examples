f9f7270c21380fdcb0b26ca2a8366f829264b197
hao0731 <ss501801@gmail.com>
2022-06-09 21:31:02 +0800

    feat: 新增全域Middleware範例

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
-import { logger } from './middlewares/functional-logger.middleware';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
@@ -13,6 +11,6 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware, logger).forRoutes(TodoController);
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
