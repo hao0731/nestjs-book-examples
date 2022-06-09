a4aec738a567757c5facd4e63de1c98eb6c5a898
hao0731 <ss501801@gmail.com>
2022-06-09 20:34:25 +0800

    feat: 調整 LoggerMiddleware

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -4,7 +4,8 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Hello Request!');
+    const { method, originalUrl } = req;
+    console.log(`[${method.toUpperCase()}] ${originalUrl}`, 'Hello Request!');
     next();
   }
 }
