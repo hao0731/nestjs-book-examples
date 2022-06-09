0b6ea46198f53949179d16c968a60be14f187381
hao0731 <ss501801@gmail.com>
2022-06-09 21:04:14 +0800

    feat: 新增指定路由與HTTP Method範例

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
-import { logger } from './middlewares/functional-logger.middleware';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
@@ -13,10 +16,11 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('todos');
-    /** 帶入 Controller 套用至底下所有路由 */
-    // consumer.apply(LoggerMiddleware).forRoutes(TodoController);
-    /** Functional Middleware */
-    // consumer.apply(logger).forRoutes('todos');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        { path: '/todos', method: RequestMethod.POST },
+        { path: '/', method: RequestMethod.GET },
+      );
   }
 }
--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('todos')
 export class TodoController {
   @Get()
   getTodos() {
     return [];
+  }
+
+  @Post()
+  createTodo(@Body() todo: Record<string, any>) {
+    return todo;
   }
 
   @Get(':id')
