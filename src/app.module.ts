404897cfb3f8d56ee5eaee2aeedf17bf57c4bc3d
hao0731 <ss501801@gmail.com>
2022-06-09 20:25:05 +0800

    feat: 新增Class Middleware範例

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
+import { logger } from './middlewares/functional-logger.middleware';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('todos');
+    /** 帶入 Controller 套用至底下所有路由 */
+    // consumer.apply(LoggerMiddleware).forRoutes(TodoController);
+    /** Functional Middleware */
+    // consumer.apply(logger).forRoutes('todos');
+  }
+}
