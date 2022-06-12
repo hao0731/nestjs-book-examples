e4cb4f60d54a4871485e2488a3866a2c3cabc599
hao0731 <ss501801@gmail.com>
2022-06-12 15:15:59 +0800

    feat: 新增Metadata裝飾器範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './decorators/roles.decorator';
+import { RoleGuard } from './guards/role.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(RoleGuard)
+  @Roles('admin')
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,11 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const handler = context.getHandler();
+    const whitelist = this.reflector.get<string[]>('roles', handler);
+    console.log(`Roles: ${whitelist.join(',')}`);
     return true;
   }
 }
