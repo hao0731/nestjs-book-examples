7a2e014cd807724c58c49bcbda8081a891ff7016
hao0731 <ss501801@gmail.com>
2022-06-14 20:48:35 +0800

    feat(user): 新增 UserModule, UserController 與 UserService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './features/todo/todo.module';
+import { UserModule } from './features/user/user.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
