9306a79c9aa7911c5c841c60ab7b07294b14d98a
hao0731 <ss501801@gmail.com>
2022-06-19 14:52:18 +0800

    feat: 新增動態處理Provider範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, OnModuleInit } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { AppService } from './app.service';
-import { StorageService } from './modules/storage/storage.service';
 
 @Controller()
-export class AppController {
-  private readonly appService: AppService;
+export class AppController implements OnModuleInit {
+  private appService: AppService;
 
-  constructor(
-    private readonly moduleRef: ModuleRef,
-    private readonly sotrageService: StorageService,
-  ) {
-    this.appService = this.moduleRef.get(AppService);
-    const storageService = this.moduleRef.get(StorageService, {
-      strict: false,
-    });
-    const isSame = storageService === this.sotrageService;
+  constructor(private readonly moduleRef: ModuleRef) {}
+
+  async onModuleInit() {
+    this.appService = await this.moduleRef.resolve(AppService);
+    const appService = await this.moduleRef.resolve(AppService);
+    const isSame = this.appService === appService;
     console.log(`is same: ${isSame}`);
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class AppService {
   getHello(): string {
     return 'Hello World!';
