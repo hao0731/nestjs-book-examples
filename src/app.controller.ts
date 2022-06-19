510444c252a4491e8b1d2c52df30c656e1d6b03a
hao0731 <ss501801@gmail.com>
2022-06-19 13:24:57 +0800

    feat: 新增取得實例範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly appService: AppService;
+
+  constructor(private readonly moduleRef: ModuleRef) {
+    this.appService = this.moduleRef.get(AppService);
+  }
 
   @Get()
   getHello(): string {
