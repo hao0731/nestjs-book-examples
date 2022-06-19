56c3c7fadb0af8039ec2bdc2717885c2c0c5a6e8
hao0731 <ss501801@gmail.com>
2022-06-19 13:26:41 +0800

    feat(modules): 新增 StorageModule 與 StorageService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StorageModule } from './modules/storage/storage.module';
 
 @Module({
-  imports: [],
+  imports: [StorageModule],
   controllers: [AppController],
   providers: [AppService],
 })
