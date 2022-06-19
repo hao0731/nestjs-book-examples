1ced320f2298fa237c29bebe6f308bb2c46f5a15
hao0731 <ss501801@gmail.com>
2022-06-19 13:41:43 +0800

    feat: 新增取得全域實例範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { AppService } from './app.service';
+import { StorageService } from './modules/storage/storage.service';
 
 @Controller()
 export class AppController {
   private readonly appService: AppService;
 
-  constructor(private readonly moduleRef: ModuleRef) {
+  constructor(
+    private readonly moduleRef: ModuleRef,
+    private readonly sotrageService: StorageService,
+  ) {
     this.appService = this.moduleRef.get(AppService);
+    const storageService = this.moduleRef.get(StorageService, {
+      strict: false,
+    });
+    const isSame = storageService === this.sotrageService;
+    console.log(`is same: ${isSame}`);
   }
 
   @Get()
