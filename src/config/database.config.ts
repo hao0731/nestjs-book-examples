a8af10b5efb9de7fac453e9beead4adbb2c65ba1
hao0731 <ss501801@gmail.com>
2022-06-23 20:14:48 +0800

    feat: 新增命名空間範例

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
-export default () => ({
-  database: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-  },
-});
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+}));
