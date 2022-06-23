7281a5f27f1d12d702d85d0e1845c216bf6f5783
hao0731 <ss501801@gmail.com>
2022-06-23 20:41:29 +0800

    feat: 新增在應用程式外部使用 CinfigService 範例

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('port');
+  await app.listen(port);
 }
 bootstrap();
