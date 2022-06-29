5ecd37a272e6698eaf6854b05d02326f426c06f1
hao0731 <ss501801@gmail.com>
2022-06-29 21:01:12 +0800

    feat: 新增預設Multter設定範例

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      fileFilter: (req, file, callback) => {
+        if (file.mimetype !== 'text/markdown') {
+          callback(null, false);
+          return;
+        }
+        callback(null, true);
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
