22cec870443e5502be0426e377f3f73a58417b03
hao0731 <ss501801@gmail.com>
2022-06-29 22:29:58 +0800

    feat: 新增客製化檔名與儲存位置的範例

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     MulterModule.register({
-      dest: './upload',
+      // dest: './upload',
+      storage: diskStorage({
+        destination: (req, file, callback) => {
+          const path = join(__dirname, '../upload');
+          callback(null, path);
+        },
+        filename: (req, file, callback) => {
+          const { originalname } = file;
+          const timestamp = Date.now();
+          const filename = `${timestamp}-${originalname}`;
+          callback(null, filename);
+        },
+      }),
     }),
   ],
   controllers: [AppController],
