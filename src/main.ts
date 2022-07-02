3c4390228e48d91f7b851b8a02a8ebdd60379ab2
hao0731 <ss501801@gmail.com>
2022-07-02 16:10:51 +0800

    feat: 新增啟用CORS範例

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // const app = await NestFactory.create(AppModule, {
+  //   cors: { origin: 'https://www.google.com' },
+  // });
+  // app.enableCors();
+  // app.enableCors({ origin: 'https://www.google.com' });
   await app.listen(3000);
 }
 bootstrap();
