97f83319ccf6731c50c577121c2313fe525387cf
hao0731 <ss501801@gmail.com>
2022-07-02 11:51:54 +0800

    feat: 新增使用HTTP Module範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('todos')
+  getTodos() {
+    return this.appService.getTodos();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { map } from 'rxjs';
+import { ITodo } from './models/todo.model';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getTodos() {
+    const baseUrl = 'https://jsonplaceholder.typicode.com';
+    return this.httpService
+      .get<ITodo>('/todos', { baseURL: baseUrl })
+      .pipe(map(({ data }) => data));
   }
 }
