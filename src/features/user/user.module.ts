1464740c5724f9b2909755550a3d262e223ab4c8
hao0731 <ss501801@gmail.com>
2022-07-14 22:35:18 +0800

    feat: 新增Mongoose Hook範例

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from '../../models/user.model';
+import { User, UserDocument, UserSchema } from '../../models/user.model';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          UserSchema.pre('save', function (this: UserDocument, next) {
+            console.log(this);
+            next();
+          });
+          return UserSchema;
+        },
+      },
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService],
