7763ecb59b56895791259bb36a8a537c56a426d2
hao0731 <ss501801@gmail.com>
2022-07-17 17:14:20 +0800

    chore: 修正 CreateUserDto 名稱

--- src/features/user/dto/create-user.dto.ts
+++ src/features/user/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class CreateeUserDto {
+export class CreateUserDto {
   @IsString()
   @MinLength(3)
   @MaxLength(20)
--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
-import { CreateeUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './models/user.model';
 
 @Injectable()
@@ -11,7 +11,7 @@ export class UserService {
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
 
-  public async createUser(dto: CreateeUserDto) {
+  public async createUser(dto: CreateUserDto) {
     const { username, email, password } = dto;
     const encrypted = await bcrypt.hash(password, 12);
     return this.userModel.create({ username, email, password: encrypted });
