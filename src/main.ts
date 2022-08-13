c815d37444ae03ec1957e49b87a36f3d6a2ad70e
hao0731 <ss501801@gmail.com>
2022-08-13 14:39:48 +0800

    feat: 新增初始化Swagger範例

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
+
+function setupSwagger(app: INestApplication) {
+  const builder = new DocumentBuilder();
+  const config = builder
+    .setTitle('TodoList')
+    .setDescription('This is a basic Swagger document.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const options: SwaggerCustomOptions = {
+    explorer: true,
+  };
+  SwaggerModule.setup('api', app, document, options);
+}
+
 bootstrap();
