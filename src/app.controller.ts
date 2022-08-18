41117e48165eb12cad834caef0c5557d59e17f81
hao0731 <ss501801@gmail.com>
2022-08-18 21:29:02 +0800

    feat: 新增Basic認證範例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBasicAuth } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiBasicAuth()
   @Get()
   getHello(): string {
     return this.appService.getHello();
